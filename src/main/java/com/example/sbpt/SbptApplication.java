ec96aa5fe3ea0fbb872e90d35d8408dfc7d40280
rajavamshi18 <62795981+rajavamshi18@users.noreply.github.com>
2026-09-02 15:51:26 +0530

    Add files via upload

---
+++ src/main/java/com/example/sbpt/SbptApplication.java
@@ -0,0 +1,13 @@
+package com.example.sbpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sbpt/SbptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sbpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
